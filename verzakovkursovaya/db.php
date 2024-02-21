a5906125f9e757d3d708288c75827e8f4983c6e8
Oropo <130141711+Oropoooo@users.noreply.github.com>
2024-02-21 13:59:54 +0300

    Add files via upload

---
+++ verzakovkursovaya/db.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "registerUser";
+
+$conn = mysqli_connect($server, $username, $password, $dbname);
+
+if(!$conn){
+    die("Connection Failed". mysql_connect_error());
+} else {
+    
+}
+
+?>
