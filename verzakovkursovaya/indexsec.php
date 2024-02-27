74b850c20aadd85bab0b666d57b2c2ce9a256d3b
Oropo <130141711+Oropoooo@users.noreply.github.com>
2024-02-27 23:08:42 +0300

    Add files via upload

---
+++ verzakovkursovaya/indexsec.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="main.css" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Main</title>
+</head>
+
+<body>
+    <div class="landing-page">
+        <div class="div">
+            <div class="div-2">
+                <div class="div-2">
+                    <div class="overlap">
+                        <div class="rectangle"></div>
+                        <div class="btn-primary">
+                            <a href="menusec.html">
+                                <div class="overlap-group">
+                                    <div class="group">
+                                        <div class="go-to-menu">GO TO MENU</div>
+                                        <img class="line" src="img/Line-1.svg" />
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <p class="taste-the-rich">Taste the rich flavor of<br />high quality sushi</p>
+                        <div class="best-sushi-in-town">Best Sushi In Town</div>
+                        <p class="we-only-use-the-five">
+                            We only use the five star quality for<br />our menu, come and get the richness in<br />every
+                            food we
+                            serve.
+                        </p>
+                    </div>
+                </div>
+                <div class="navbar">
+                    <div class="navbar-2">
+                        <a href="menusec.html">
+                            <div class="text-wrapper-menu">
+                                <p class="p-menu">Menu</p>
+                            </div>
+                        </a>
+                        <a href="reviewssec.html">
+                            <div class="text-wrapper-reviews">
+                                <p class="p-reviews">Reviews</p>
+                            </div>
+                        </a>
+                        <a href="aboutsec.html">
+                            <div class="text-wrapper-about">
+                                <p class="p-about">About</p>
+                            </div>
+                        </a>
+                        <a href="staffsec.html">
+                            <div class="text-wrapper-staff">
+                                <p class="p-staff">Staff</p>
+                            </div>
+                        </a>
+                        <a href="supplierssec.html">
+                            <div class="text-wrapper-suppliers">
+                                <p class="p-suppliers">Suppliers</p>
+                            </div>
+                        </a>
+                        <a href="partnerssec.html">
+                            <div class="text-wrapper-partners">
+                                <p class="p-partners">Partners</p>
+                            </div>
+                        </a>
+                        <a href="contactssec.html">
+                            <div class="text-wrapper-contacts">
+                                <p class="p-contacts">Contacts</p>
+                            </div>
+                        </a>
+                    </div>
+                    <a href="booking.php">
+                        <div class="group-wrapper">
+                            <div class="group-2">
+                                <div class="reservation">RESERVATION</div>
+                                <img class="img" src="img/line.svg" />
+                            </div>
+                        </div>
+                    </a>
+                    <a href="indexsec.php">
+                        <img class="logo" src="img/logo-restaurant.png" />
+                    </a>
+                </div>
+            </div>
+            <div class="today-special">
+                <div class="special-menu">Special Menu</div>
+                <div class="group-3">
+                    <div class="text-wrapper-5">Today’s Special</div>
+                    <p class="special-menu-oftenly">
+                        Special menu oftenly comes different everyday,<br />this is our special food for today
+                    </p>
+                    <div class="overlap-wrapper">
+                        <div class="overlap-2">
+                            <div class="overlap-group-wrapper">
+                                <div class="overlap-group-2">
+                                    <div class="text-wrapper-6">Tuna Sushi</div>
+                                    <img class="star" src="img/star-1.svg" />
+                                    <img class="star-2" src="img/star-2.svg" />
+                                    <img class="star-3" src="img/star-3.svg" />
+                                    <img class="star-4" src="img/star-4.svg" />
+                                    <img class="star-5" src="img/star-5.svg" />
+                                    <p class="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut
+                                        imperdiet lectus.</p>
+                                </div>
+                            </div>
+                            <div class="group-5">
+                                <div class="overlap-3">
+                                    <div class="text-wrapper-7">Salmon Sushi</div>
+                                    <img class="star-6" src="img/star-1.svg" />
+                                    <img class="star-7" src="img/star-2.svg" />
+                                    <img class="star-8" src="img/star-3.svg" />
+                                    <img class="star-9" src="img/star-4.svg" />
+                                    <img class="star-10" src="img/star-5.svg" />
+                                    <img class="mask-group" src="img/sushi6.png" />
+                                    <p class="text-wrapper-8">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut imperdiet
+                                        lectus.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="group-6">
+                                <div class="overlap-4">
+                                    <div class="text-wrapper-6">Just Sushi</div>
+                                    <img class="star" src="img/star-1.svg" />
+                                    <img class="star-2" src="img/star-2.svg" />
+                                    <img class="star-3" src="img/star-3.svg" />
+                                    <img class="star-4" src="img/star-4.svg" />
+                                    <img class="star-5" src="img/star-5.svg" />
+                                    <p class="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut
+                                        imperdiet lectus.</p>
+                                </div>
+                            </div>
+                            <img class="mask-group-2" src="img/sushi1.png" />
+                            <img class="mask-group-3" src="img/sushi3.png" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="rectangle-wrapper">
+                <div class="rectangle-2"></div>
+            </div>
+            <footer class="footer">
+                <div class="overlap-13">
+                    <img class="mask-group-13" src="img/finedining3 1.png" />
+                    <div class="rectangle-10"></div>
+                    <a href="indexsec.php">
+                        <img class="logo-2" src="img/logo.png" />
+                    </a>
+                    <a href="booking.php">
+                        <div class="group-29">
+                            <div class="overlap-group-4">
+                                <div class="text-wrapper-25">Reserve a Table</div>
+                            </div>
+                        </div>
+                    </a>
+                    <p class="we-ready-to-have-you">We ready to have you <br />the best dining experiences</p>
+                    <div class="group-30">
+                        <div class="group-31">
+                            <p class="text-wrapper-26">Jendral Sudirman Street Pahoman<br />Bandar Lampung, Lampung,
+                                35222</p>
+                            <img class="map-pin-fill" src="img/map-pin-fill.svg" />
+                        </div>
+                        <div class="group-32">
+                            <div class="text-wrapper-26">Call us:<br />+0721 471 285</div>
+                            <img class="phone-fill" src="img/phone-fill.svg" />
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+</body>
+
+</html>
