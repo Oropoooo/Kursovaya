98539bcdee910e650cc1e14ccb7ff25d7563f7bb
Oropo <130141711+Oropoooo@users.noreply.github.com>
2024-03-05 12:02:54 +0300

    Add files via upload

---
+++ verzakovkursovaya/pages/main/index.js
@@ -0,0 +1,25 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+        } else {
+            change.target.classList.remove('element-show');
+        }
+    });
+}
+
+let options = {
+    threshold: [0.2]
+};
+
+// Создаем наблюдатель для каждого класса
+let observers = [];
+
+['.overlap-wrapper', '.star', '.star-2', '.star-3', '.star-4', '.star-5', '.star-6', '.star-7', '.star-8', '.star-9', '.star-10'].forEach(selector => {
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = document.querySelectorAll(selector);
+    elements.forEach(elm => {
+        observer.observe(elm);
+    });
+    observers.push(observer);
+});
